58923ada79809bd5d412edc70c8e04bc6d9fce8f
Kristijan Horvatić <111249835+pansport@users.noreply.github.com>
2023-06-09 11:59:47 +0200

    Add files via upload

---
+++ 05-travel-list/src/App.js
@@ -0,0 +1,61 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+  { id: 2, description: "Charger", quantity: 2, packed: false },
+  { id: 2, description: "Towels", quantity: 5, packed: true },
+  { id: 2, description: "Shampoo", quantity: 3, packed: false },
+];
+
+export default function App() {
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
+
+function Logo() {
+  return <h1>🌴 FAR AWAY 💼</h1>;
+}
+
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your 😍 trip?</h3>
+    </div>
+  );
+}
+
+function PackingList() {
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
+}
+
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>💼 You have 3 items on your list, and you already packed 1 item</em>
+    </footer>
+  );
+}
